3beaed4a6f309a8b0098abc4c6e33d64df865698
ontoligent <ontoligent@gmail.com>
2017-12-03 20:51:49 -0500

    Added PoloScraper

--- polo2/__init__.py
+++ polo2/__init__.py
@@ -8,6 +8,7 @@ from polo2.polo_text import PoloText
 from polo2.polo_report import PoloReport
 from polo2.polo_retro import PoloRetro
 from polo2.polo_gensim import PoloGensim
+from polo2.polo_scraper import PoloScraper
 
 #__all__ = ['PoloDb', 'PoloFile', 'PoloConfig', 'PoloCorpus', 'PoloMallet', 'PoloMath', 'PoloText', 'PoloReport', 'PoloRetro', 'PoloGensim']
-__all__ = 'PoloDb PoloFile PoloConfig PoloCorpus PoloMallet PoloMath PoloText PoloReport PoloRetro PoloGensim'.split()
+__all__ = 'PoloDb PoloFile PoloConfig PoloCorpus PoloMallet PoloMath PoloText PoloReport PoloRetro PoloGensim PoloScraper'.split()
